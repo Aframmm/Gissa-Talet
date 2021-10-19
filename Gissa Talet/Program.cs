cac811af031900966e29b07a4433ed63096809be
Aframmm <89247875+Aframmm@users.noreply.github.com>
2021-10-19 13:12:53 +0200

    Add files via upload

---
+++ Gissa Talet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+ 
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int AntalFörsök  =0;
+           Random random = new Random();
+           int Tal = random.Next(1, 101);
+ 
+           Console.WriteLine("Gissa mitt tal. Välj ett tal mellan 1 och 100.");
+ 
+           while (true) {
+               int Försök = int.Parse(Console.ReadLine());
+ 
+               if(Försök == Tal) {
+                   Console.WriteLine("Du gissade rätt");
+                   Console.WriteLine("Det tog dig " + AntalFörsök + " försök.");
+                   break;
+ 
+               } else if(Försök < Tal) {
+                   Console.WriteLine("Försök högre");
+                   AntalFörsök += 1;
+ 
+                  } else {
+                   Console.WriteLine("Försök lägre");
+                   AntalFörsök += 1;
+                  }
+               
+ 
+           }
+        }
+    }
+}
+ 
